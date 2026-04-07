e877b9067af8c11e896557cf120e042a68b7164a
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-07 22:27:13 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
+import { navLinks } from "../../constants";
 
 const Navbar = () => {
   return (
     <nav>
-        <div>
-            <a href="#home" className="flex items-center gap-2">
-                Velvet Pour
-            </a>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          Velvet Pour
+        </a>
 
-            <ul>
-
-            </ul>
-        </div>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
-  )
-}
-
-export default Navbar
+  );
+};
+export default Navbar;

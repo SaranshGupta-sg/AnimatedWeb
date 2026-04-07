92ecc842fa2ce79df76079fa97c0e789ab1883b3
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-07 22:52:47 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import gsap from 'gsap';
+import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
-import Navbar from './components/Navbar.jsx';
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText); //this line make sure that both plugins are ready to use globally
 
@@ -9,8 +10,9 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
